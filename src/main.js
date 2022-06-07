2e4d102c945c003a2ec5551581b5a5963d83b9a1
w3soto <soto.ms@gmail.com>
2022-06-07 11:10:06 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,41 @@
+
+import "./sass/main.scss";
+
+
+// NAVIGATION
+const nav = document.querySelector(".navigation");
+
+const navBtns = document.querySelectorAll(".navigation__button, .navigation__link");
+navBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+      nav.classList.toggle("active");
+  });
+});
+
+document.addEventListener("scroll", () => {
+    const el =  window;
+    if (el.scrollY > 0.05 * el.innerHeight) {
+        nav.classList.add("navigation--active");
+    }
+    else {
+        nav.classList.remove("navigation--active");
+    }
+}, {passive: true});
+
+// FADE IN
+const fadeInObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(e => {
+        if (e.isIntersecting) {
+            e.target.classList.add("appear");
+            observer.unobserve(e.target);
+        }
+    });
+}, {
+    threshold: 0,
+    rootMargin: "0px 0px -128px 0px"
+});
+
+document.querySelectorAll(".fade-in")
+    .forEach(el => fadeInObserver.observe(el));
+
+
